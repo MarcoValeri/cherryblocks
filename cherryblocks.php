1350306142fb6f8f0e8f37d84f4bd89c75c602a5
Marco Valeri <marco.valeri@cherrythinking.com>
2023-12-08 12:55:20 +0000

    First commit: setup the plugin

---
+++ cherryblocks.php
@@ -0,0 +1,11 @@
+/**
+ * Plugin Name:       Cherry Blocks
+ * Description:       Cherry Blocks for custom WordPress blocks
+ * Requires at least: 5.8
+ * Requires PHP:      8.0
+ * Version:           0.1.0
+ * Author:            Marco Valeri
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       cherryblocks
+ */
